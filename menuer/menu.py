67a0b93a426cff9a63eaa69ebd66ae61bf57f187
James <jrhenderson11@gmail.com>
2018-12-09 21:45:57 +0000

    Added tab indentation support

--- menuer/menu.py
+++ menuer/menu.py
@@ -6,12 +6,12 @@ class Menu():
 	menu_items = {}
 	asciiart = ""
 
-	def __init__(self, asciiart, menu_items):
+	def __init__(self, asciiart, menu_items, tab="		"):
 		if not isinstance(menu_items, OrderedDict):
 			raise Exception("menu_items is not an OrderedDictionary")
 		self.asciiart = asciiart
 		self.menu_items = menu_items
-
+		self.tab = tab
 	def printgreen(self, text):
 		print('\033[92m' + text + '\033[0m')
 
@@ -23,7 +23,7 @@ class Menu():
 		
 	def print_menu_items(self):
 		for i, s in enumerate(list(self.menu_items.keys())):
-			print(" [{}] {}".format(i+1, s))
+			print(self.tab + "[{}] {}".format(i+1, s))
 		print("")
 
 	def menu_loop(self):
@@ -33,13 +33,19 @@ class Menu():
 		self.print_intro()
 		print("\n"	)
 		self.print_menu_items()
-		num = int(input())
+		try:
+			num = int(input())
+		except ValueError:
+			num = -1
 		while not (num>0 and num<len(self.menu_items)+1):
 			print('\033[u')
 			self.print_intro()
-			print("	   \033[31m\033[1m  invalid input \033[0m\n")
+			print("\033[31m\033[1m		     invalid input \033[0m\n")
 			self.print_menu_items()
-			num = int(input('\033[K'))
+			try:
+				num = int(input('\033[K'))
+			except ValueError:
+				num = -1
 		num-=1
 		list(self.menu_items.values())[num]()
 
